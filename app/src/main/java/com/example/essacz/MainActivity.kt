7f820e45fd4e15fd725a07af162a856ca22904e7
Kamfeuszek <florczakk01@gmail.com>
2024-03-04 16:44:07 +0100

    Dodano 1 button pokazujący "Hello World"

--- app/src/main/java/com/example/essacz/MainActivity.kt
+++ app/src/main/java/com/example/essacz/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.essacz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun click1(view: View) {
+        var displaytext : TextView = findViewById(R.id.textView1)
+        displaytext.setText("Hello World")
     }
 }
